44eb943bb99fcaea91abbbfee16c732dbeab005d
Wakhungu N Rebecca <rebeccanakhanu@gmail.com>
2023-03-13 08:40:00 +0300

    completed ssiggnment

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solutions in this file
+function writeCards(names, event){
+    const messages = [];
+    for (let i = 0;
+        i < names.length;
+        i++) {
+    messages.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`)
+        }
+        return messages;
+  }
+  writeCards(["Ada", "Brendan", "Ali"], "surprise")
+  function countDown(num){
+  while(num>=0){
+      console.log(num);
+      num--;
+  }
+  }
+  countDown(10);
